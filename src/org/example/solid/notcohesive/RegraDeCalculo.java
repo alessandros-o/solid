5b71004a70769c44687ab3d01958171d53172c9b
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2022-09-26 13:42:31 -0300

    Refatorando código com princípio da responsabilidade única.

---
+++ src/org/example/solid/notcohesive/RegraDeCalculo.java
@@ -0,0 +1,6 @@
+package org.example.solid.notcohesive;
+
+public interface RegraDeCalculo {
+
+    public double calcula(Funcionario funcionario);
+}
--- src/org/example/solid/notcohesive/enums/Cargo.java
+++ src/org/example/solid/notcohesive/enums/Cargo.java
@@ -1,7 +1,21 @@
 package org.example.solid.notcohesive.enums;
 
+import org.example.solid.notcohesive.DezOuVintePorCento;
+import org.example.solid.notcohesive.QuinzeOuVinteCincoPorcento;
+import org.example.solid.notcohesive.RegraDeCalculo;
+
 public enum Cargo {
-    DESENVOLVEDOR,
-    DBA,
-    TESTER
+    DESENVOLVEDOR(new DezOuVintePorCento()),
+    DBA(new QuinzeOuVinteCincoPorcento()),
+    TESTER(new QuinzeOuVinteCincoPorcento());
+
+    private RegraDeCalculo regra;
+
+    Cargo(RegraDeCalculo regra) {
+        this.regra = regra;
+    }
+
+    public RegraDeCalculo getRegra() {
+        return regra;
+    }
 }
