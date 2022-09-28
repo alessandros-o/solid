849d456d273d934eb34ae0efa5b7267402fe7e26
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2022-09-28 11:00:19 -0300

    commit classes da atividade 6 sem refatoração

---
+++ src/org/example/solid/atividade6/rh/ValidacaoException.java
@@ -0,0 +1,11 @@
+package org.example.solid.atividade6.rh;
+
+public class ValidacaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidacaoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
---
+++ src/org/example/solid/atividade6/rh/model/Cargo.java
@@ -0,0 +1,10 @@
+package org.example.solid.atividade6.rh.model;
+
+public enum Cargo {
+
+	ASSISTENTE,
+	ANALISTA,
+	ESPECIALISTA,
+	GERENTE;
+
+}
